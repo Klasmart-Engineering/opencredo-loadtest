9dcb1de797c363a9e9ae1126a2b0ae8f0aac57b6
Matt Revell <matthew.revell@opencredo.com>
2021-12-16 12:11:21 +0000

    Add utils with login setup function

---
+++ utils/setup.js
@@ -0,0 +1,62 @@
+import http from 'k6/http';
+import { check, group, sleep, fail } from 'k6';
+
+export const options = {
+  vus: 100,
+  duration: '1m',
+  thresholds: {
+    http_req_duration: ['p(99)<1500'], // 99% of requests must complete below 1.5s
+  },
+};
+
+const userAgent = 'Mozilla/5.0 (X11; Fedora; Linux x86_64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/96.0.4664.93 Safari/537.36';
+
+const defaultHeaders = {
+  pragma: 'no-cache',
+  'user-agent': userAgent
+};
+
+const APIHeaders = Object.assign({
+  accept: 'application/json',
+  'content-type': 'application/json',
+}, defaultHeaders)
+
+const AMS_URL = 'https://auth.dev.badanamu.net';
+const APP_URL = 'loadtest.kidsloop.live';
+const USERNAME = 'max.flintoff+testlogin@opencredo.com';
+const PASSWORD = __ENV.PASSWORD;
+
+export function loginSetup() {
+  const accessToken = group('Login to AMS', function () {
+    const loginPay = JSON.stringify({
+
+      email: USERNAME,
+      pw: PASSWORD,
+      deviceId: "loadtest",
+      deviceName: "k6",
+    });
+
+    const loginParams = {
+      headers: APIHeaders
+    }
+
+    http.options(`${AMS_URL}/v1/login`);
+
+    const loginResp = http.post(`${AMS_URL}/v1/login`, loginPay, loginParams);
+    check(loginResp, {
+      'is status 200': (r) => r.status === 200,
+    });
+
+    return loginResp.json('accessToken');
+  });
+
+  const authPayload = JSON.stringify({
+    token: accessToken
+    });
+
+  const transferResp = http.post(`https://auth.${APP_URL}/transfer`, authPayload, {
+      headers: APIHeaders
+    });
+
+  return transferResp;
+};
