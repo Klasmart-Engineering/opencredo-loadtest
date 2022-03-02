35db7aa9ef1bd1f8c8b7d2742e7ed258d229c71c
Oscar Dovao <oscar.dovao@opencredo.com>
2022-03-02 11:20:56 +0000

    xapi tests using defaultRateOptions (scenarios)

--- kidsloop-xapi/test-xapi.js
+++ kidsloop-xapi/test-xapi.js
@@ -1,31 +1,8 @@
 import { loginSetup } from '../utils/setup.js'
 import { xapiTest } from './functions.js'
+import {defaultRateOptions} from "../utils/common.js";
 
-export const options = {
-  vus: 1,
-  iterations: 1,
-
-  thresholds: {
-    http_req_duration: ['p(99)<1000'], // 99% of requests must complete below 1s
-    iteration_duration: ['p(95)<3000'] // 95% of the iteration duration below 2s
-  },
-
-  ext: {
-    loadimpact: {
-      projectID: 3560234,
-      distribution: {
-        mumbaiDistribution: {
-          loadZone: 'amazon:gb:london',
-          percent: 50
-        },
-        portlandDistribution: {
-          loadZone: 'amazon:ie:dublin',
-          percent: 50
-        },
-      }
-    }
-  },
-}
+export const options = defaultRateOptions
 
 const APP_URL = __ENV.APP_URL
 const USERNAME = __ENV.USERNAME
