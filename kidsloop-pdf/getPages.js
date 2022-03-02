93bf1d6bf72b1970ca70ecb8e54d419f2286f53f
Matt Revell <matthew.revell@opencredo.com>
2022-03-02 17:11:49 +0000

    Add PDF service tests

---
+++ kidsloop-pdf/getPages.js
@@ -0,0 +1,49 @@
+import { loginSetup } from '../utils/setup.js'
+import { initCookieJar } from '../utils/common.js'
+import { prerender } from './common.js'
+import { getPagesTest } from './functions.js'
+
+const APP_URL = __ENV.APP_URL
+const USERNAME = __ENV.USERNAME
+const AMSENV = __ENV.AMSENV
+const RATE = __ENV.RATE || 1
+
+const pdfEndpoint = `https://api.${APP_URL}/pdf`;
+const pdfFileName = '61ead76d1708577bc731ed93.pdf';
+
+// Configure options
+export const options = {
+  ext: {
+    loadimpact: {
+      projectID: 3560234,
+    }
+  },
+  scenarios: {
+    pages: {
+      executor: 'constant-arrival-rate',
+      rate: RATE,
+      duration: '30s',
+      preAllocatedVUs: 100,
+      maxVUs: 10000,
+    }
+  }
+};
+
+export function setup() {
+  let amsEnv = AMSENV
+  if (!amsEnv) {
+    amsEnv = 'dev'
+  }
+
+  const accessCookie = loginSetup(APP_URL, USERNAME, amsEnv);
+  prerender(accessCookie, pdfEndpoint, pdfFileName);
+
+  return {
+    accessCookie: accessCookie
+  }
+}
+
+export default function main(data) {
+  initCookieJar(data.accessCookie);
+  getPagesTest(pdfEndpoint, pdfFileName);
+}
