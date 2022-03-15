41f65fd7831222894091ce6a29a3227e9eb3e5aa
Matt Revell <matthew.revell@opencredo.com>
2022-03-15 10:32:33 +0000

    Add multi-user test and env fix for H5P test

--- kidsloop-h5p/tests/contentParams.js
+++ kidsloop-h5p/tests/contentParams.js
@@ -1,13 +1,14 @@
 import http           from 'k6/http';
 import {chooseRandom} from './_functions.js';
 import {apiHeaders}   from './envs/_shared.js';
-import {contentIds}   from './envs/loadtest-k8s.js';
+import {contentIds}   from './envs/_contentIds.js';
+import {APP_URL}      from '../../utils/env.js'
 
 // GET  /h5p/play/:contentId
 // GET  https://h5p.loadtest-k8s.kidsloop.live/h5p/play/61e5ab9293c3e70013dfa9a
 export function contentParamsTest(h5pEndpoint, token) {
 
-    const url = `${h5pEndpoint}/h5p/params/${chooseRandom(contentIds)}?jwt=${token}`
+    const url = `${h5pEndpoint}/h5p/params/${chooseRandom(contentIds[APP_URL])}?jwt=${token}`
 
     const response = http.get(url, {
         headers: apiHeaders
